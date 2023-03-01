e5eb16fb1c2d506c4d0448565ba42de3bec5ed83
temba23 <tembasherpa80@gmail.com>
2023-03-01 21:43:08 +0545

    assignment

---
+++ main.py
@@ -0,0 +1,24 @@
+# #write a python program to check whether the input number is prime or not.
+# n = int(input("enter a number"))
+# count = 0
+# i = 1
+# while (i <= n):
+#     if (n % i == 0):
+#         count = count + 1
+#     i = i + 1
+# if (count==2):
+#      print("it is a prime number")
+# else:
+#     print("it is not a prime number")
+#write a program to calculate the difference between a given number and 17. if the number is greater than 17, return
+#twice the absoulte number
+a = int(input("enter a number"))
+b = (a - 17)
+if ( a > 17 ):
+    print(a ** 2)
+else:
+    print("b")
+
+
+
+
